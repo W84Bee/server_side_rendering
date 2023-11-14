94997b68c03c9e374369fd8af9cc37aa68f1370b
W84Bee <122640885+W84Bee@users.noreply.github.com>
2023-11-14 13:48:19 -0600

    Public Directory
    Creating the public folder and sending it to the browser

--- build/bundle.js
+++ build/bundle.js
@@ -7059,12 +7059,14 @@ var _Home2 = _interopRequireDefault(_Home);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express2.default)();
-// using the require module syntax
 
+app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
   var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
 
-  res.send(content);
+  var html = '\n    <html>\n      <head></head>\n      <body>\n          <div>' + content + '</div>\n          <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
+
+  res.send(html);
 });
 
 app.listen(3000, function () {
--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 
 const app = express();
-// using the require module syntax
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   const content = renderToString(<Home />);
 
-  res.send(content);
+  const html = `
+    <html>
+      <head></head>
+      <body>
+          <div>${content}</div>
+          <script src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+
+  res.send(html);
 });
 
 app.listen(3000, () => {
