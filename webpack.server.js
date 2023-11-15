6b35e1b2f65831aa533ecbda2b82eb77df424374
Brandon Alexander <bjvonalexander@gmail.com>
2023-11-14 18:40:46 -0600

    Ignoring Files with Webpack
    - ignores files on the server side that doesn't need to be used

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base.js');
+const webpackNodeExternals = require('webpack-node-externals');
 
 const config = {
   // Inform webpack that were building a bundle
@@ -14,7 +15,9 @@ const config = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }
+  },
+
+  externals: [webpackNodeExternals()]
 };
 
 module.exports = merge(baseConfig, config);
