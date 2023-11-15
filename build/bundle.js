0c84179be82326941e2a445f825b8f4fe14e7a66
Brandon Alexander <bjvonalexander@gmail.com>
2023-11-14 20:09:18 -0600

    More on Route Configuration
    Creating the Static Routes for the server
    Adding in the information for the static Routing

--- build/bundle.js
+++ build/bundle.js
@@ -90,7 +90,7 @@ var app = (0, _express2.default)();
 
 app.use(_express2.default.static('public'));
 app.get('/', function (req, res) {
-  res.send((0, _renderer2.default)());
+  res.send((0, _renderer2.default)(req));
 });
 
 app.listen(3000, function () {
@@ -172,8 +172,12 @@ var _Routes2 = _interopRequireDefault(_Routes);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function () {
-  var content = (0, _server.renderToString)(_react2.default.createElement(Home, null));
+exports.default = function (req) {
+  var content = (0, _server.renderToString)(_react2.default.createElement(
+    _reactRouterDom.StaticRouter,
+    { location: req.path, context: {} },
+    _react2.default.createElement(_Routes2.default, null)
+  ));
 
   return '\n    <html>\n      <head></head>\n      <body>\n          <div id="root">' + content + '</div>\n          <script src="bundle.js"></script>\n      </body>\n    </html>\n  ';
 };
